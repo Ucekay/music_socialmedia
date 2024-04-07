5bcd721d8446a38ce0c1da220caf67b67481e71a
Ucekay <u331951c@ecs.osaka-u.ac.jp>
2024-04-08 00:27:28 +0900

    Update ArticleSummaryCard.tsx

--- src/components/ArticleSummaryCard.tsx
+++ src/components/ArticleSummaryCard.tsx
@@ -78,7 +78,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginVertical: 16,
+    marginVertical: 8,
   },
   summaryContainer: {
     backgroundColor: 'white',
