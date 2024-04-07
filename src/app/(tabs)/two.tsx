624f2c6ef2084e1de46d7437d0e270e7a6116888
Ucekay <u331951c@ecs.osaka-u.ac.jp>
2024-04-08 08:41:30 +0900

    Update two.tsx

--- src/app/(tabs)/two.tsx
+++ src/app/(tabs)/two.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { FlatList } from 'react-native';
+import { FlashList } from '@shopify/flash-list';
 import ArticleSummaryCard from '@/src/components/ArticleSummaryCard';
 import articleData from '@/src/assets/articleData';
 
 export default function TabTwoScreen() {
   return (
-    <FlatList
+    <FlashList
       data={articleData}
       renderItem={({ item }) => <ArticleSummaryCard article={item} />}
       keyExtractor={(item) => item.articleID}
+      estimatedItemSize={317}
       contentContainerStyle={{ padding: 16 }}
     />
   );
