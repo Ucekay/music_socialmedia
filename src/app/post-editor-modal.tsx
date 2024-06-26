10a0d6b95cf8c91eb91ac238204cc4b5b81d5b58
kaeru61 <160379169+kaeru61@users.noreply.github.com>
2024-06-27 08:16:17 +0900

    feat: post-editor
    postの投稿画面を実装しました。
    写真の撮影と読み込み、編集に対応しています

--- src/app/post-editor-modal.tsx
+++ src/app/post-editor-modal.tsx
@@ -1,6 +1,20 @@
 // PostScreen.js
-import React, { useState, useRef} from 'react';
-import { View, TextInput, Button, StyleSheet, Text, useColorScheme, ScrollView, Platform, Animated, Pressable, FlatList,Image as RNImage } from 'react-native';
+import React, { useState, useRef, useEffect} from 'react';
+import { View,
+         TextInput, 
+         Button,
+         StyleSheet, 
+         Text, 
+         useColorScheme, 
+         ScrollView, 
+         Platform, 
+         Animated as Animated1, 
+         Pressable, 
+         FlatList, 
+         Modal,
+         ActivityIndicator,
+         GestureResponderEvent
+        } from 'react-native';
 import userData from '../assets/userData';
 import { Image } from 'expo-image';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -12,21 +26,72 @@ import { useNavigation } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import Icon from "react-native-vector-icons/AntDesign"
 import ImageAspectKept from '../components/ImageAspectKept';
+import { BlurView } from 'expo-blur';
+import * as ImagePicker from 'expo-image-picker'
+import ImageCropPicker from 'react-native-image-crop-picker';
+import  Animated, {FadeIn, FadeOut} from 'react-native-reanimated'
 
 const PostEditorModal = () => {
   const [text, setText] = useState('');
   const insets = useSafeAreaInsets();
   const [inputHeight, setInputHeight] = useState(40); // 初期の高さを設定
   const [postHeight, setPostHeight] = useState(60);
-  const [imagelayout, setImagelayout] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [loading, setLoading] = useState(false)
   const colorScheme = useColorScheme();
   const navigation = useNavigation();
-  const animatedHeight = useRef(new Animated.Value(0)).current; 
+  const animatedHeight = useRef(new Animated1.Value(0)).current; 
   
-  const [images, setImages] = useState(['https://assets.st-note.com/production/uploads/images/118769644/rectangle_large_type_2_dfac95fd8fb3ca429c798b9656b2c54d.png?width=2000&height=2000&fit=bounds&format=jpg&quality=85',
-  'https://assets.st-note.com/production/uploads/images/119567308/rectangle_large_type_2_64867acd466f0affc7c9fa6020ecd244.png?width=2000&height=2000&fit=bounds&format=jpg&quality=85',
-  'https://m.media-amazon.com/images/I/71GJUXIF6QL._UF1000,1000_QL80_.jpg'
-  ]);
+  const [images, setImages] = useState<string[]>([]);
+
+  const pickImage = async () => {
+    setErrorMessage('');
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsMultipleSelection: true,
+      selectionLimit: 4 - images.length,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setLoading(true);
+      for (let i = 0; i < result.assets.length; i++) 
+      setImages(images => [...images, result.assets[i].uri])
+      setLoading(false);
+    } else {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestCameraPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  const takePhoto = async () => {
+    try {
+      const result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setLoading(true);
+        setImages(images => [...images, result.assets[i].uri])
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error('Error taking picture:', error);
+      setLoading(false);
+    }
+  };
 
   const textColor = Color[colorScheme ?? 'light'].text;
   const secondaryTextColor = Color[colorScheme ?? 'light'].secondaryText;
@@ -44,7 +109,7 @@ const PostEditorModal = () => {
     setPostHeight(height-60); // Viewの高さを状態に保存
 
     // 高さをアニメーションで変更
-    Animated.timing(animatedHeight, {
+    Animated1.timing(animatedHeight, {
       toValue: height,
       duration: 300, // アニメーションの持続時間（ミリ秒）
       useNativeDriver: false, // height のアニメーションには native driver は使えない
@@ -58,12 +123,39 @@ const PostEditorModal = () => {
   };
 
   const handleLayoutChangeLine = () => {
-    Animated.timing(animatedHeight, {
+    Animated1.timing(animatedHeight, {
       toValue: postHeight,
       duration: 100, // アニメーションの持続時間（ミリ秒）
       useNativeDriver: false, // height のアニメーションには native driver は使えない
     }).start();
   };
+
+   
+  const openCropEditor = async (num: number, uri: string) => {
+    try {
+      setLoading(true)
+      const cropped = await ImageCropPicker.openCropper({
+        path: uri,
+        cropperToolbarTitle: '画像を編集',
+        mediaType: 'photo',
+        freeStyleCropEnabled: true
+      });
+      setImages(images.map((images, index) => (index === num ? cropped.path : images )));
+    } catch (e: any) {
+      console.log(e.message);
+      if (e.message === 'User cancelled image selection') {
+        setErrorMessage(
+          '画像のクロップがキャンセルされたので、画像を読み込めませんでした。'
+        );
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleCropEditor = (num: number, uri: string) => {
+    openCropEditor(num, uri);
+  }
 
   return (
     
@@ -75,14 +167,22 @@ const PostEditorModal = () => {
       <View style={styles.header}>
         <Text style={[styles.headertitle, {color: textColor}]}>新規ポスト</Text>
       </View>
+      <Modal animationType='fade' transparent={true} visible={loading}>
+        <View style={styles.dialog}>
+          <BlurView tint={'systemMaterial'} style={styles.dialogInner}>
+            <Text style={styles.text}>画像を読み込んでいます</Text>
+            <ActivityIndicator />
+          </BlurView>
+        </View>
+      </Modal>
       <View style={styles.editorHeader}>
           <View style={{flexDirection: 'column'}}>
             <Image source={userData[0].userAvatarUrl} style={styles.avatorimage} />
-            <Animated.View 
+            <Animated1.View 
             style={[styles.line, {height: animatedHeight}]}
             onLayout={handleLayoutChangeLine}/>
           </View>
-            <Animated.View
+            <Animated1.View
               style={[
                 { flexDirection: 'column' },
                 { flex: 1 },
@@ -103,26 +203,46 @@ const PostEditorModal = () => {
                     }}
                     scrollEnabled={false}
                 />
-              {imagelayout===true ? (
+              {images.length > 0 ? (
+              <Animated.View 
+              entering={FadeIn}
+              exiting={FadeOut}>
               <FlatList
               horizontal
               data={images}
               keyExtractor={(item, index) => index.toString()}
               renderItem={({ item, index }) => (
-                  <ImageAspectKept style={styles.image} url={item} height={200}>
-                    <Pressable
-                      style={styles.deleteButton}
-                      onPress={() => handleDeleteImage(index)}
-                    >
-                      <Icon name="delete" size={24} color="#fff" />
-                    </Pressable>
-                  </ImageAspectKept>
+                <Animated.View 
+                entering={FadeIn}
+                exiting={FadeOut}>
+                  <Pressable onPress={(e) => {handleCropEditor(index, item)}}>
+                    <ImageAspectKept style={styles.image} uri={item} height={200}>
+                      <Pressable
+                        style={styles.deleteButton}
+                        onPress={() => handleDeleteImage(index)}
+                      >
+                        <Icon name="delete" size={24} color="#fff" />
+                      </Pressable>
+                    </ImageAspectKept>
+                  </Pressable>
+                </Animated.View>
               )}
-            />) : (
+            />
+            </Animated.View>) : (
               null
               )}
-            <IconAntDesign name='paperclip' size={20} style={styles.Icon} onPress={() => setImagelayout(!imagelayout)}/>
-            </Animated.View>
+            {images.length !== 4 ? (
+            <View style={{flexDirection: 'row', gap: 20}}>
+              <IconAntDesign name='paperclip' size={20} style={styles.Icon} onPress={pickImage}/>
+              <IconAntDesign name='camerao' size={20} style={styles.Icon} onPress={takePhoto}/>
+            </View>
+            ):(
+            <View style={{flexDirection: 'row', gap: 20}}>
+              <IconAntDesign name='paperclip' size={20} style={styles.Icon}/>
+              <IconAntDesign name='camerao' size={20} style={styles.Icon}/>
+            </View>
+            )}
+            </Animated1.View>
         </View>
         </ScrollView>
         <BgView
@@ -152,7 +272,7 @@ const PostEditorModal = () => {
           </View>
           <View style={styles.buttonContainer}>
             <FontAwesome6 name='check' size={16} color={textColor} />
-            <Button title='Post' onPress={() => {}} color={textColor} />
+            <Button title='Post' onPress={() => {handlePost}} color={textColor} />
           </View>
         </View>
       </BgView>
@@ -166,7 +286,6 @@ export default PostEditorModal
 const styles = StyleSheet.create({
   container: {
     paddingTop: 20,
-    paddingRight: 20,
     flex: 1
   },
   header: {
@@ -200,6 +319,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginTop: 10,
     textAlignVertical: 'top',
+    marginRight: 20
   },
   avatorimage: {
     width: 30,
@@ -216,7 +336,6 @@ const styles = StyleSheet.create({
   },
   headerLeft: {
     flexDirection: 'row',
-    marginRight: 8,
     alignItems: 'baseline',
     gap: 8,
   },
@@ -258,5 +377,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 4,
     paddingHorizontal: 16,
+  },
+  dialog: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dialogInner: {
+    width: '70%',
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 12,
+    borderRadius: 16,
+    borderCurve: 'continuous',
+    overflow: 'hidden',
+  },
+  text: {
+    fontSize: 17,
   },
 });
--- src/components/ImageAspectKept.tsx
+++ src/components/ImageAspectKept.tsx
@@ -3,7 +3,7 @@ import { Image } from 'expo-image';
 import { Image as RNImage } from 'react-native'
 
 interface Props {
-    url: string,
+    uri: string,
     height: number,
     children: JSX.Element,
     style?: any,
@@ -11,12 +11,12 @@ interface Props {
 
 const ImageAspectKept = (props: Props) : JSX.Element => {
     const [width, setWidht] = useState(200)
-    RNImage.getSize(props.url, (originalWidth, originalHeight) => {
+    RNImage.getSize(props.uri, (originalWidth, originalHeight) => {
         const newWidth = (Number(originalWidth*props.height/originalHeight));
         setWidht(newWidth);
       });
   return(
-    <Image source={props.url} style={[props.style, {height: props.height, width: width} ]}>{props.children}</Image>
+    <Image source={{uri: props.uri}} style={[props.style, {height: props.height, width: width} ]}>{props.children}</Image>
   )
 }
 
