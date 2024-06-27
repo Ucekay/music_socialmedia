2dbd1341212953f94a30dd810b1723075d56b6b6
kaeru61 <160379169+kaeru61@users.noreply.github.com>
2024-06-27 14:32:10 +0900

    feat/today-song-list

--- src/app/(tabs)/today.tsx
+++ src/app/(tabs)/today.tsx
@@ -1,22 +1,31 @@
-import { Button, Pressable, View } from 'react-native';
+import { Button, Pressable, View, FlatList, StyleSheet } from 'react-native';
 import { useState } from 'react';
-
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import Text from '@/src/components/ThemedText';
 import BgView from '@/src/components/ThemedBgView';
 import { Link } from 'expo-router';
+import todaySongData from '@/src/assets/todaySongData';
+import TodaySongCardForList from '@/src/components/TodaySongCardForList';
 
 const TodayScreen = () => {
+  const tabBarHeight = useBottomTabBarHeight();
   return (
-    <BgView
-      style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}
-    >
-      <Pressable>
-        <Link href='/today-song-modal'>
-          <Text>open modal</Text>
-        </Link>
-      </Pressable>
+    <BgView style={{ marginBottom: tabBarHeight, flex:1}}>
+            <FlatList 
+            data={todaySongData}
+            renderItem={({item}) => (<TodaySongCardForList todaySong={item}/>)}
+            numColumns={2}
+            columnWrapperStyle={styles.column}/>
     </BgView>
   );
 };
+
+const styles = StyleSheet.create({
+  column: {
+    justifyContent: 'space-around',
+    marginTop: 10,
+  }
+})
+
 
 export default TodayScreen;
