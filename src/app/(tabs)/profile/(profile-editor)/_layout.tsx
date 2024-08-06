3f347282fef808b4c4088a7279ca42c55e894c48
kaeru61 <160379169+kaeru61@users.noreply.github.com>
2024-08-07 01:20:33 +0900

    feat: profile reply-editor

---
+++ src/app/(tabs)/profile/(profile-editor)/_layout.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Stack } from 'expo-router';
+import { ProfileStateProvider } from '@/src/contexts/ProfileEditor';
+
+const App = () => {
+  return (
+    <ProfileStateProvider>
+      <Stack>
+        <Stack.Screen
+          name='index'
+          options={{
+            headerShown: false,
+            presentation: 'modal',
+            title: 'Peofile Editor',
+            gestureEnabled: false,
+        }}/>
+        <Stack.Screen
+          name='name-editor-modal'
+          options={{
+            headerShown: false,
+            presentation: 'modal',
+            title: 'Name Editor',
+            gestureEnabled: true,
+        }}/>
+        <Stack.Screen
+          name='id-editor-modal'
+          options={{
+            headerShown: false,
+            presentation: 'modal',
+            title: 'Id Editor',
+            gestureEnabled: true,
+        }}/>
+        <Stack.Screen
+          name='bio-editor-modal'
+          options={{
+            headerShown: false,
+            presentation: 'modal',
+            title: 'Bio Editor',
+            gestureEnabled: true,
+        }}/>
+        <Stack.Screen
+          name='favoriteArtists-editor-modal'
+          options={{
+            headerShown: false,
+            presentation: 'modal',
+            title: 'Favorite Artists Editor',
+            gestureEnabled: true,
+        }}/>
+      </Stack>
+    </ProfileStateProvider>
+  );
+};
+
+export default App;
