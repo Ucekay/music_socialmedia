fd84a688c1b86cac053e25628f2e97515dd36079
Ucekay <u331951c@ecs.osaka-u.ac.jp>
2024-08-25 18:33:22 +0900

    chore

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,13 +1,12 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
-import { supabase, supabaseUrl } from '../backend/lib/supabase';
+import { supabase } from '../backend/lib/supabase';
 import * as SecureStore from 'expo-secure-store';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Session, AuthChangeEvent,  } from '@supabase/supabase-js'; 
+import { Session, AuthChangeEvent } from '@supabase/supabase-js';
 import Constants from 'expo-constants';
 
-const USER_ID_KEY =  Constants.expoConfig?.extra?.USER_ID_KEY;
-const SECURE_STORE_KEY =  Constants.exxpoConfig?.extra?.SECURE_STORE_KEY;
-
+const USER_ID_KEY = Constants.expoConfig?.extra?.USER_ID_KEY;
+const SECURE_STORE_KEY = Constants.exxpoConfig?.extra?.SECURE_STORE_KEY;
 
 interface AuthContextType {
   session: Session | null;
@@ -16,7 +15,7 @@ interface AuthContextType {
   signUpWithEmail: (email: string, password: string) => Promise<void>;
   signInWithOAuth: (provider: 'apple' | 'google') => Promise<void>;
   signOut: () => Promise<void>;
-  children?: React.ReactNode 
+  children?: React.ReactNode;
 }
 
 export const AuthContext = createContext<AuthContextType>({
@@ -28,7 +27,9 @@ export const AuthContext = createContext<AuthContextType>({
   signOut: async () => {},
 });
 
-export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [session, setSession] = useState<Session | null>(null);
   const [loading, setLoading] = useState(true);
 
@@ -37,7 +38,6 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
       const storedSession = await SecureStore.getItemAsync(SECURE_STORE_KEY);
       if (storedSession) {
         try {
-
           const { data, error } = await supabase.auth.refreshSession(
             JSON.parse(storedSession).refresh_token
           );
@@ -45,13 +45,13 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
             console.error('セッションの復元に失敗しました:', error);
           } else {
             // data.session が null かどうかをチェック
-          if (data.session) {
-            setSession(data.session);
-          } else {
-          // セッションが null の場合の処理（例：エラー状態をセットするなど）
-          console.error('セッション情報が取得できませんでした');
-          // 必要に応じて setSession(null) を行う
-        }
+            if (data.session) {
+              setSession(data.session);
+            } else {
+              // セッションが null の場合の処理（例：エラー状態をセットするなど）
+              console.error('セッション情報が取得できませんでした');
+              // 必要に応じて setSession(null) を行う
+            }
           }
         } catch (error) {
           console.error('SecureStoreからの読み込みエラー:', error);
@@ -61,12 +61,14 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     };
     checkSession();
 
-    const { data: { subscription } } = supabase.auth.onAuthStateChange(
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(
       async (_event: AuthChangeEvent, session: Session | null) => {
-        setSession(session); 
+        setSession(session);
         if (session) {
           try {
-             // セッションが有効になったら、ユーザーIDをAsyncStorageに保存
+            // セッションが有効になったら、ユーザーIDをAsyncStorageに保存
             await AsyncStorage.setItem(USER_ID_KEY, session.user.id);
             await SecureStore.setItemAsync(
               SECURE_STORE_KEY,
@@ -76,7 +78,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
             console.error('SecureStoreへの保存エラー:', error);
           }
         } else {
-          await AsyncStorage.removeItem(USER_ID_KEY)
+          await AsyncStorage.removeItem(USER_ID_KEY);
           await SecureStore.deleteItemAsync(SECURE_STORE_KEY);
         }
       }
@@ -85,9 +87,12 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     return () => subscription.unsubscribe();
   }, []);
 
-   // メールアドレスとパスワードによるログイン
-   const signInWithEmail = async (email: string, password: string) => {
-    const { error } = await supabase.auth.signInWithPassword({ email, password });
+  // メールアドレスとパスワードによるログイン
+  const signInWithEmail = async (email: string, password: string) => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
     if (error) {
       console.error(error);
       // エラー処理を実装する
--- src/contexts/ProfileEditor.tsx
+++ src/contexts/ProfileEditor.tsx
@@ -1,31 +1,63 @@
 import React, { createContext, useState } from 'react';
 
 type ProfileStateType = {
-    name: string;
-    setName: React.Dispatch<React.SetStateAction<string>>;
-    id: string;
-    setId: React.Dispatch<React.SetStateAction<string>>;
-    bio: string;
-    setBio: React.Dispatch<React.SetStateAction<string>>;
-    userAvatar: string;
-    setUserAvatar: React.Dispatch<React.SetStateAction<string>>;
-    tag: string[];
-    setTag: React.Dispatch<React.SetStateAction<string[]>>;
-  };
+  name: string;
+  setName: React.Dispatch<React.SetStateAction<string>>;
+  id: string;
+  setId: React.Dispatch<React.SetStateAction<string>>;
+  bio: string;
+  setBio: React.Dispatch<React.SetStateAction<string>>;
+  userAvatar: string;
+  setUserAvatar: React.Dispatch<React.SetStateAction<string>>;
+  tag: string[];
+  setTag: React.Dispatch<React.SetStateAction<string[]>>;
+};
 
 // Contextを作成
-export const ProfileEditorContext = createContext<ProfileStateType | undefined>(undefined);
+export const ProfileEditorContext = createContext<ProfileStateType | undefined>(
+  undefined
+);
 
 // Providerコンポーネントを作成
-export const ProfileStateProvider = ({ children }) => {
+export const ProfileStateProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
   const [name, setName] = useState('Initial State');
   const [id, setId] = useState('');
-  const [bio, setBio] = useState('')
-  const [userAvatar, setUserAvatar] = useState('https://api.dicebear.com/8.x/bottts/png')
-  const [tag, setTag] = useState(['Kana Nishino', 'Ikimono Gakari', 'AI', 'GReeeeN', 'Daichi Miura', 'Miwa', 'Fujifabric', 'Little Glee Monster', 'Sakanaction', 'Shota Shimizu'])
+  const [bio, setBio] = useState('');
+  const [userAvatar, setUserAvatar] = useState(
+    'https://api.dicebear.com/8.x/bottts/png'
+  );
+  const [tag, setTag] = useState([
+    'Kana Nishino',
+    'Ikimono Gakari',
+    'AI',
+    'GReeeeN',
+    'Daichi Miura',
+    'Miwa',
+    'Fujifabric',
+    'Little Glee Monster',
+    'Sakanaction',
+    'Shota Shimizu',
+  ]);
 
   return (
-    <ProfileEditorContext.Provider value={{ name, setName, id, setId, bio ,setBio, tag, setTag, userAvatar, setUserAvatar }}>
+    <ProfileEditorContext.Provider
+      value={{
+        name,
+        setName,
+        id,
+        setId,
+        bio,
+        setBio,
+        tag,
+        setTag,
+        userAvatar,
+        setUserAvatar,
+      }}
+    >
       {children}
     </ProfileEditorContext.Provider>
   );
