74c01aee832d1c59e825b1ae7770ca5aee2c2394
Ucekay <u331951c@ecs.osaka-u.ac.jp>
2024-09-08 00:30:39 +0900

    chore: setup biome

--- src/test/jest.setup.ts
+++ src/test/jest.setup.ts
@@ -1,15 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl =  '"https://kuipswqwsrdwkjwyplwv.supabase.co"';
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1aXBzd3F3c3Jkd2tqd3lwbHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc2ODU5MzUsImV4cCI6MjAzMzI2MTkzNX0.sLRv71ZoT7ZS9mWiIOqhchEK2iStBkjG_DHZC7PwAew";
+const supabaseUrl = '"https://kuipswqwsrdwkjwyplwv.supabase.co"';
+const supabaseKey =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1aXBzd3F3c3Jkd2tqd3lwbHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc2ODU5MzUsImV4cCI6MjAzMzI2MTkzNX0.sLRv71ZoT7ZS9mWiIOqhchEK2iStBkjG_DHZC7PwAew';
 
 (globalThis as any).supabase = createClient(supabaseUrl, supabaseKey);
 
 // テスト前にSupabaseのテーブルをクリアするなど
 beforeAll(async () => {
-  await (globalThis as any).supabase.from('your_table_name').delete().cascade();
+	await (globalThis as any).supabase.from('your_table_name').delete().cascade();
 });
 
 afterAll(async () => {
-  // テスト後のクリーンアップ処理
+	// テスト後のクリーンアップ処理
 });
