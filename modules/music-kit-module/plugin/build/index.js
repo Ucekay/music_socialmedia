e320df69b9d62c8bb1880c67f8573731e8d1d446
Ucekay <u331951c@ecs.osaka-u.ac.jp>
2024-09-10 06:57:48 +0900

    chore: build plugin

---
+++ modules/music-kit-module/plugin/build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const withMusicKitModule = (config) => {
+    console.log('MusikKitModule plugin');
+    return config;
+};
+exports.default = withMusicKitModule;
