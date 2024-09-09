d12e6a1bf7cf991f696b3c7afcffbf77aae78aa1
kaeru61 <160379169+kaeru61@users.noreply.github.com>
2024-09-10 03:16:22 +0900

    refactor/backend

--- src/backend/schema/error.ts
+++ src/backend/schema/error.ts
@@ -8,6 +8,9 @@ class CustomError extends Error {
 // 入力が不正な場合のエラー
 export const BadRequestError = new CustomError('入力が不正です', 400);
 
+// 不明なエラー
+export const InternalError = new CustomError('サーバーエラーが発生しました', 500);
+
 // 挿入系エラー
-export const CreateArticleError = new CustomError('記事の作成に失敗しました', 500);
-export const CreatePostError = new CustomError('投稿の作成に失敗しました', 500);
+export const CreateArticleError = new CustomError('記事の作成に失敗しました', 1000);
+export const CreatePostError = new CustomError('投稿の作成に失敗しました', 1001);
