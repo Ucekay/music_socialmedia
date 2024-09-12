2ef91c77d52f8ca03d5ed24c4fd285ab67dd3282
Ucekay <u331951c@ecs.osaka-u.ac.jp>
2024-09-12 19:09:39 +0900

    feat: check apple music subscription

---
+++ modules/music-kit-module/ios/MusicSubscription.swift
@@ -0,0 +1,23 @@
+//
+//  MusicSubscriptionManager.swift
+//  Pods
+//
+//  Created by 木村優介 on 9/12/24.
+//
+
+import MusicKit
+import Foundation
+
+@available(iOS 15.0, *)
+class MusicSubscriptionManager: NSObject {
+    func checkSubscription()async -> (canPlayCatalogContent: Bool, canBecomeSubscriber: Bool, hasCloudLibraryEnabled: Bool) {
+        var musicSubscription: MusicSubscription?
+        for await subscripton in MusicSubscription.subscriptionUpdates {
+            musicSubscription = subscripton
+        }
+        let canPlayCatarogContent = musicSubscription?.canPlayCatalogContent ?? false
+        let canBecomeSubscriber = musicSubscription?.canBecomeSubscriber ?? false
+        let hasCloudLibraryEnabled = musicSubscription?.hasCloudLibraryEnabled ?? false
+        return (canPlayCatarogContent, canBecomeSubscriber, hasCloudLibraryEnabled)
+    }
+}
